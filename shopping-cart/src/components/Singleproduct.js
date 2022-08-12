b815d72ab351535fbac8b24d1382171fa9c9abec
chalana1995 <chalanakalpitha72@gmail.com>
2022-08-12 11:04:41 +0530

    create remove add to cart part

--- shopping-cart/src/components/Singleproduct.js
+++ shopping-cart/src/components/Singleproduct.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import { Button, Card } from 'react-bootstrap'
+import { CartState } from '../context/Context'
 import Rating from './Rating'
 
 const Singleproduct = ({ prod }) => {
+
+
+  const {
+    state,
+    dispatch
+  } = CartState();
+
   return (
     <div className='products'>
       <Card>
@@ -18,12 +26,29 @@ const Singleproduct = ({ prod }) => {
             )}
             <Rating rating={prod.ratings} />
           </Card.Subtitle>
-          <Button variant='danger'>
-            Remove from cart
-          </Button>
-          <Button disabled={!prod.inStock}>
-            {!prod.inStock ? "Out of Stock" : "Add to Cart"}
-          </Button>
+          {state.cart.some((p) => p.id === prod.id) ? (
+            <Button variant='danger'
+            onClick={() => {
+              dispatch({
+                type: "REMOVE_FROM_CART",
+                payload: prod
+              })
+            }}
+            >
+              Remove from cart
+            </Button>
+          ) : (
+            <Button
+            onClick={() => {
+              dispatch({
+                type: "ADD_TO_CART",
+                payload: prod
+              })
+            }}
+            disabled={!prod.inStock}>
+              {!prod.inStock ? "Out of Stock" : "Add to Cart"}
+            </Button>
+          )}
         </Card.Body>
       </Card>
     </div>
